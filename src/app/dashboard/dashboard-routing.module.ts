086add2b55691f18a3cb7ef99155f27697a84e01
Bmis Kemgoum <bmisarts@gmail.com>
2023-11-08 18:21:38 +0100

    Latest style additions

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainComponent } from "./main/main.component";
+import { HomeComponent } from "./home/home.component";
 
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ]
   },
 ];
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './layout/nav/nav.component';
@@ -7,6 +8,14 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { NavRightComponent } from './layout/nav/nav-right/nav-right.component';
 import { SearchComponent } from './layout/nav/search/search.component';
+import { HomeComponent } from './home/home.component';
+import { RequisitionsComponent } from './home/requisitions/requisitions.component';
+import { RecoveryComponent } from './home/recovery/recovery.component';
+import { RecognisationComponent } from './home/recognisation/recognisation.component';
+import { NewUpdateComponent } from './home/new-update/new-update.component';
+import { GeolocationComponent } from './home/geolocation/geolocation.component';
+import { StatDatasComponent } from './home/stat-datas/stat-datas.component';
+import { NotifyComponent } from './home/notify/notify.component';
 
 
 
@@ -17,11 +26,20 @@ import { SearchComponent } from './layout/nav/search/search.component';
     FooterComponent,
     SidebarComponent,
     NavRightComponent,
-    SearchComponent
+    SearchComponent,
+    HomeComponent,
+    RequisitionsComponent,
+    RecoveryComponent,
+    RecognisationComponent,
+    NewUpdateComponent,
+    GeolocationComponent,
+    StatDatasComponent,
+    NotifyComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgApexchartsModule,
   ]
 })
 export class DashboardModule { }
